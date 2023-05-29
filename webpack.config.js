00a006cff9ab3f4bf740c177e39e6a05619e1f7e
zohra-neda <zohra.neda@gmail.com>
2023-05-29 21:11:46 +0430

    Set up the project with Webpack and Jest

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'JavaScript Capstone Project',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+};
