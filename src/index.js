b888b8a3c2d8ebc65f7dba3a86d9d9b580b1fdbc
Nahid1911 <nahid.raihan@gmail.com>
2023-05-31 00:45:42 +0600

    add popup

--- src/index.js
+++ src/index.js
@@ -9,3 +9,51 @@ const displayAllMovies = async () => {
 };
 
 window.addEventListener('load', displayAllMovies);
+const parentElem = document.getElementById('liContainer');
+parentElem.addEventListener('click', (event) => {
+  if (event.target.matches('.btn')) {
+    const newDiv = document.createElement('div');
+    newDiv.className = "mainContainer";
+    newDiv.innerHTML = `
+    <div class="mainContainer1">
+
+    <div class="closeBtd flex">
+        <img src="./modules/close_icon.png" alt="main image">
+    </div>
+
+    <div class="movieDetails flex">
+        <div class="imgDiv">
+           <img src="./modules/rsz_popup_mobile_thumbnail.png" alt="main image">
+        </div>
+        <h2>Movie Name</h2>
+        <div class="movieInfo flex">
+            <div class="otherInfo flex">
+                <span>Language</span>
+                <span>Genres</span>
+            </div>
+            <div class="otherInfo flex">
+                <span>Episode Length</span>
+                <span>Genres</span>
+            </div>
+        </div>
+        <div class="comments flex">
+            <h3>Comments</h3>
+            <div class="commentsDisplay flex">
+                <span>Nahid : Love this movie</span>
+            </div>
+        </div>
+        <div class="addComment flex">
+            <h3>Add a comment</h3>
+            <div class="commentField flex">
+                <input type="text" class="name" placeholder="Your name">
+                <textarea name="" id="" cols="30" rows="5" placeholder="Your insights"></textarea>
+                <button>Comment</button>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+    document.body.appendChild(newDiv);
+  }
+});
+// console.log(parentElem);
