17e183d47f6313f466d3e5398cb03966d26cddd9
zohra-neda <zohra.neda@gmail.com>
2023-05-30 13:25:54 +0430

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './styles/main.css';
 import './images/logo1.jpg';
-import './styles/main.css';
 import displayMovies from './modules/displayMovies.js';
 import getMovies from './modules/api.js';
 
