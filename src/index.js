9c0d1db15c8a075d9a322ad3ff5de324e10b560b
zohra-neda <zohra.neda@gmail.com>
2023-05-30 13:23:36 +0430

    Add section for displaying all cards

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './styles/main.css';
 import './images/logo1.jpg';
+import './styles/main.css';
+import displayMovies from './modules/displayMovies.js';
+import getMovies from './modules/api.js';
+
+const displayAllMovies = async () => {
+  const display = await getMovies();
+  displayMovies(display);
+};
+
+window.addEventListener('load', displayAllMovies);
