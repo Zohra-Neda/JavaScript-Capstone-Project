7ef6c476501ba40ca614ca3ab33645483a93408d
zohra-neda <zohra.neda@gmail.com>
2023-05-31 18:04:54 +0430

    Add two files addLikes.js and showLikes.js inside modules directory

--- src/modules/displayMovies.js
+++ src/modules/displayMovies.js
@@ -1,21 +1,20 @@
 const cardsContainer = document.querySelector('.cards-display-section');
 
 const displayMovies = async (shows) => {
-  cardsContainer.innerHTML = '';
   for (let i = 0; i < 20 && i < shows.length; i += 1) {
     const show = shows[i];
 
     cardsContainer.innerHTML += `
         <li id="movie-${show.id}" class="movie-item">
           <img src=${show.image.medium} alt="movie-image" class="movie-image" />
-          <div class="likes">
+          <div class="likes-container">
             <h4 class="movie-title">${show.name}</h4>
-            <div class="heart">
-              <i class="fa-regular fa-heart" id=${show.id}></i>
-              <p id="${show.id}"> <span class="like-numbers"></span></p>
+            <div class="icon">
+            <i class="fa-sharp fa-regular fa-heart" data-id=${show.id}></i>
+            <p data-id="${show.id}"><span class="like-rates"></span></p>
             </div>
           </div>
-          <div class="description">
+          <div>
             <button class="btn" id="${show.id}">Comments</button>
           </div>
         </li>
